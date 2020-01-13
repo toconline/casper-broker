a698acd4e412d6529b4e0889fd3bbcf7eba4ab02
AndreSilva1993 <andreemanuel.1993@gmail.com>
2020-01-13 11:22:34 +0000

    Implements timeout in all the requests and includes the meta information from the response

--- casper-broker.js
+++ casper-broker.js
@@ -17,10 +17,10 @@ class CasperBroker extends PolymerElement {
     };
   }
 
-  async get (url) { return this.__formatResponse(await this.__request('GET', url)); }
-  async delete (url) { return this.__formatResponse(await this.__request('DELETE', url)); }
-  async post (url, body) { return this.__formatResponse(await this.__request('POST', url, body)); }
-  async patch (url, body) { return this.__formatResponse(await this.__request('PATCH', url, body)); }
+  async get (url, timeoutInMilliseconds) { return this.__formatResponse(await this.__request('GET', url, undefined, timeoutInMilliseconds)); }
+  async post (url, body, timeoutInMilliseconds) { return this.__formatResponse(await this.__request('POST', url, body, timeoutInMilliseconds)); }
+  async patch (url, body, timeoutInMilliseconds) { return this.__formatResponse(await this.__request('PATCH', url, body, timeoutInMilliseconds)); }
+  async delete (url, body, timeoutInMilliseconds) { return this.__formatResponse(await this.__request('DELETE', url, body, timeoutInMilliseconds)); }
 
   /**
    * Performs an HTTP request to the ngix-broker API.
@@ -28,10 +28,14 @@ class CasperBroker extends PolymerElement {
    * @param {String} method The request's HTTP verb.
    * @param {String} requestUrl The request's URL.
    * @param {Object} requestBody The request's body.
+   * @param {Number} timeoutInMilliseconds The request's timeout in milliseconds.
    */
-  async __request (method, requestUrl, requestBody) {
+  async __request (method, requestUrl, requestBody, timeoutInMilliseconds) {
+    const abortController = new AbortController();
+
     const fetchSettings = {
       method: method,
+      signal: abortController.signal,
       headers: new Headers({
         'Authorization': `Bearer ${this.__readCookieValue('casper_session')}`,
         'Content-Type': 'application/vnd.api+json'
@@ -44,6 +48,8 @@ class CasperBroker extends PolymerElement {
     }
 
     try {
+      setTimeout(() => { abortController.abort(); }, timeoutInMilliseconds);
+
       const fetchResponse = await fetch(`${this.apiBaseUrl}/${requestUrl}`, fetchSettings);
 
       return await fetchResponse.json();
@@ -67,9 +73,15 @@ class CasperBroker extends PolymerElement {
     if (!response.data) return;
 
     // Flatten the nginx-broker response.
-    return response.data.constructor.name === 'Object'
-      ? { id: response.data.id, ...response.data.attributes }
-      : response.data.map(item => ({ id: item.id, ...item.attributes }));
+    if (response.data.constructor.name === 'Object') {
+      return !response.hasOwnProperty('meta')
+        ? { id: response.data.id, ...response.data.attributes }
+        : { data: { id: response.data.id, ...response.data.attributes }, meta: { ...response.meta } };
+    } else {
+      return !response.hasOwnProperty('meta')
+        ? response.data.map(item => ({ id: item.id, ...item.attributes }))
+        : { data: response.data.map(item => ({ id: item.id, ...item.attributes })), meta: { ...response.meta } };
+    }
   }
 
   /**
